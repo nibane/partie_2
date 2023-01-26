2287fb8b546713332203d6907204f7237d79f8b9
nibane <azari46165@gmail.com>
2023-01-26 12:01:28 +0100

    début

---
+++ exo_1.php
@@ -0,0 +1,15 @@
+<h1>Exercice 1<h/1>
+
+<p>Créer une fonction personnalisée convertirMajRouge()permettant de transformer une chaîne de caractère passée en argument en majuscules et en rouge.Vous devrez appeler la fonction comme suit: convertirMajRouge($texte);Affichage(si $texte = «Mon texte en paramètre»)MON TEXTE EN PARAMETRE<p/>
+
+<h2>Résultat</h2>
+
+<?php
+
+function convertirMajRouge($arg){
+    $arg = stroupper($arg);
+    return $arg
+}
+
+echo convertirMajRouge("sdfsdf");
+?>
