31a3669895a52d56ed25a6295daf03266be606d0
nibane <azari46165@gmail.com>
2023-01-29 19:01:16 +0100

    formulaire html php

--- exo_4.php
+++ exo_4.php
@@ -1,6 +1,6 @@
 <h1>Exercice 4<h/1>
 
-<p>A partir de l'exercice 2, ajouter une colonne supplémentaire dans le tableau HTML qui contiendra le lien hypertexte de la page Wikipédia de la capitale (le lien hypertexte devra s'ouvrir dans un nouvel onglet et le tableau sera trié par ordre alphabétique de la capitale).On admet que l'URL de la page Wikipédia de la capitale adopte la forme:https://fr.wikipedia.org/wiki/Le tableau passé en argument sera le suivant:$capitales= array ("France"=>"Paris","Allemagne"=>"Berlin","USA"=>"Washington","Italie"=>"Rome","Espagne"=>"Madrid");AffichagePaysCapitaleLien wikiALLEMAGNEBerlinLienESPAGNEMadridLienFRANCEParisLienITALIERomeLienUSAWashingtonLien<p/>
+<p>A partir de l'exercice 2, ajouter une colonne supplémentaire dans le tableau HTML qui contiendra le lien hypertexte de la page Wikipédia de la capitale (le lien hypertexte devra s'ouvrir dans un nouvel onglet et le tableau sera trié par ordre alphabétique de la capitale).On admet que l'URL de la page Wikipédia de la capitale adopte la forme:https://fr.wikipedia.org/wiki/Le tableau passé en argument sera le suivant:$capitales= array ("France"=>"Paris","Allemagne"=>"Berlin","USA"=>"Washington","Italie"=>"Rome","Espagne"=>"Madrid");AffichagePaysCapitaleLien wikiALLEMAGNEBerlinLienESPAGNEMadridLienFRANCEParisLienITALIERomeLienUSAWashingtonLien</p>
 
 <h2>Résultat</h2>
 
---
+++ exo_5.php
@@ -0,0 +1,27 @@
+<h1>Exercice 5<h/1>
+
+<p>Créer  une  fonction  personnalisée  permettant  de créer  un  formulaire  de  champs  de  texte  en précisant le nom des champs associés.Exemple:$nomsInput = array("Nom","Prénom","Ville");afficherInput($nomsInput);Affichage</p>
+
+<h2></h2>
+
+
+
+
+
+
+
+<?php
+
+$nomInput = ["Nom","Prénom","Ville"];
+
+function afficherInput(array $arg){
+    echo "<form>";
+    foreach ($arg as $form ) {
+        echo "<div><label>$form</label><br><input type='string'><br></div>";
+    }
+    echo "</form>";
+}
+
+afficherInput($nomInput);
+
+?>
