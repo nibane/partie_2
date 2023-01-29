af74810e607483d80b9ea654dd87b78ae78065cc
nibane <azari46165@gmail.com>
2023-01-29 22:25:34 +0100

    menu deroulant php html

--- exo_6.php
+++ exo_6.php
@@ -1,4 +1,4 @@
-<h1>Exercice 5<h/1>
+<h1>Exercice 6<h/1>
 
 <p>Créer une fonction personnalisée permettant de remplir une liste déroulante à partir d'un tableau de valeurs.Exemple:$elements = array("Monsieur","Madame","Mademoiselle");alimenterListeDeroulante($elements);Affichage</p>
 
@@ -7,6 +7,15 @@
 
 <?php
 
+$elements = ["Monsieur","Madame","Mademoiselle"];
 
+function alimenterListeDeroulante ($arg){
+    echo "<select name = box>";
+    foreach($arg as $option)
+        echo"<option>".$option."</option>";
+    echo "</select>";
+}
+
+alimenterListeDeroulante($elements)
 
 ?>
