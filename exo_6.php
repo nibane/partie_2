bc6643163dff98f8d1189dba513c601de2a80e01
nibane <azari46165@gmail.com>
2023-01-29 22:25:19 +0100

    html php menu deroulant

---
+++ exo_6.php
@@ -0,0 +1,12 @@
+<h1>Exercice 5<h/1>
+
+<p>Créer une fonction personnalisée permettant de remplir une liste déroulante à partir d'un tableau de valeurs.Exemple:$elements = array("Monsieur","Madame","Mademoiselle");alimenterListeDeroulante($elements);Affichage</p>
+
+<h2></h2>
+
+
+<?php
+
+
+
+?>
