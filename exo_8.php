4eea8f074dbf36fbc087426af40e2be25cac10b4
nibane <azari46165@gmail.com>
2023-02-01 12:00:01 +0100

    ajout variable = 0 , ajout fonction , ajout while

---
+++ exo_8.php
@@ -0,0 +1,20 @@
+<h1>Exercice 8<h/1>
+
+<p>Créer une fonction personnalisée permettant de générer des cases à cocher. On pourra préciser dans le tableau associatif si la case est cochée ou non.</p>
+
+<h2></h2>
+
+
+<?php
+
+$nb_depart = 0
+
+
+function repeterImage($nb){
+    while
+
+}
+
+
+
+?>
