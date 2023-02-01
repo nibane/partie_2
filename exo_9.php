5a427ddf850bb2c413760049c006ea2a70b08538
nibane <azari46165@gmail.com>
2023-02-01 14:02:49 +0100

    creation d'un foreach dans une fonction

---
+++ exo_9.php
@@ -0,0 +1,15 @@
+<h1>Exercice 8<h/1>
+
+<p>Créer une fonctionpersonnalisée permettant d'afficher des boutons radio avec un tableau de valeurs en paramètre("Monsieur","Madame","Mademoiselle").</p>
+
+<h2></h2>
+
+
+<?php
+
+function afficherRadio(...$numbers) {
+    foreach (...$numbers as $key)  
+        
+}
+
+?>
