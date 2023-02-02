da66b5d60d61dae679c47f9bace4a6e158c3d17f
nibane <azari46165@gmail.com>
2023-02-02 11:17:05 +0100

    class IntlDateFormatter utilisé est parametrer

---
+++ exo_11.php
@@ -0,0 +1,27 @@
+<h1>Exercice 11<h/1>
+
+<p>Ecrire une fonction personnalisée qui affiche une date en français (en toutes lettres) à partir d'une chaîne de caractère représentant une date.Exemple:formaterDateFr("2018-02-23");Affichage vendredi 23 février 2018</p>
+
+<h2></h2>
+
+
+<?php
+
+$fmt = new IntlDateFormatter(
+    "fr-FR", 
+    IntlDateFormatter::FULL, 
+    IntlDateFormatter::NONE, 
+    'Etc/UTC', 
+    IntlDateFormatter::GREGORIAN, 
+    'EEEE, dd MMMM y'
+);
+
+// // parse
+// $ts = $fmt->parse('Lundi, 08 Juillet 2013 09:09');
+// echo $ts; 
+
+$date = new DateTime();
+
+echo $fmt->format($date);
+
+?>
