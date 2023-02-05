bb8af1e68c2e1646805fd3d044f0de8d656843d0
nibane <azari46165@gmail.com>
2023-02-05 19:12:11 +0100

    ajout abstract final protected

--- glosaire.php
+++ glosaire.php
@@ -12,3 +12,20 @@ Personne::hello();
 //affiche 'salut'
 pas de this dans une méthode static
 on peut faire pareill avec un atribut
+
+créer un atribut avec protected
+au lieu de public, fera que seul 
+les class filles pouron avoir 
+accés a ces atribut
+
+
+mettre final devant une class empechera un
+heritage suplementaire
+pareille pour les methodes 
+
+mettre abstract devant une class
+est les methodes de cette class
+fera quel pouront etre utilisé
+comme base pour des filles
+mais quel ne pouront pas etre 
+utilisé
