dacf45328f9b66f2cc201f782a4051cab6bc9e2a
nibane <azari46165@gmail.com>
2023-02-06 09:50:15 +0100

    affichage de $ve1 object issue de la class fille VoitureElec

--- exo_14.php
+++ exo_14.php
@@ -1,4 +1,4 @@
-<h1>Exercice 12<h/1>
+<h1>Exercice 14<h/1>
 
 <p>Créer une classe Voiture possédant 2 propriétés(marque et modèle) ainsi qu'une classe VoitureElec qui hérite(extends) de la classe Voiture et qui a une propriété supplémentaire (autonomie).Instancier une voiture «classique» et une voiture «électrique»ayant les caractéristiques suivantes:Peugeot 408: $v1 = new Voiture("Peugeot","408");BMW i3150: $ve1 = new VoitureElec("BMW","I3",100);Votre programme de test devra afficher les informations des 2 voitures de la façon suivante: echo $v1->getInfos()."<br/>";echo $ve1->getInfos()."<br/>";</p>
 
@@ -55,6 +55,9 @@ class VoitureElec extends voiture{
 $v1 = new Voiture("Peugeot", "408");
 echo $v1->get_Infos();
 
+
+$ve1 = new VoitureElec("BMW", "I3", 100);
+echo $ve1->get_Infos();
 
 
 
