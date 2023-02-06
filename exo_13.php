60186daefb6bce566e835f034867dd2aa359c553
nibane <azari46165@gmail.com>
2023-02-06 08:47:41 +0100

    trasfert des commandes dans la part 2

--- exo_13.php
+++ exo_13.php
@@ -117,14 +117,4 @@ class Voiture{
 
 
 
-$v1 = new Voiture("Batmobile", "Char de combat", 1);
-
-echo $v1;
-echo $v1->demarrer();
-echo $v1->accelerer(30);
-echo $v1->accelerer(30); 
-echo $v1->stop();
-echo $v1->info();
-
-
 ?>
